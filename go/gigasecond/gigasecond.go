535ead5fe8cb1d9f07308e32775aa85b72c0be43
hrpatel <pate3900+github@gmail.com>
2016-11-20 12:00:32 -0500

    add imports, implement solution

--- go/gigasecond/gigasecond.go
+++ go/gigasecond/gigasecond.go
@@ -1,10 +1,14 @@
 // Package clause.
 package gigasecond
 
+import (
+  "time"
+)
+
 // Constant declaration.
-const testVersion = ? // find the value in gigasecond_test.go
+const testVersion = 4
 
 // API function.  It uses a type from the Go standard library.
-func AddGigasecond(time.Time) time.Time
-
-// Reviewers don't think much of stub comments.  Replace or remove.
+func AddGigasecond(curr_time time.Time) time.Time {
+  return curr_time.Add(1000000000 * time.Second)
+}
