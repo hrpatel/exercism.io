c04a9b6f53f91468439983f541f1d6f9eb35858e
hrpatel <pate3900+github@gmail.com>
2016-11-21 21:46:42 -0500

    implement solution for hamming exercise

--- go/hamming/hamming.go
+++ go/hamming/hamming.go
@@ -1,7 +1,27 @@
 package hamming
 
+import (
+  "errors"
+)
+
 const testVersion = 5
 
-func Distance(a, b string) (int, error) {
-	// your code here!
+func Distance(a, b string) (dist int, err error) {
+  // initialize constants
+  dist = 0
+
+  // error out on strings with different lengthts
+  if len(a) != len(b) {
+    err = errors.New("string lengthts dont match!")
+    return
+  }
+
+	// iterate through each array
+  for idx := range a {
+    if a[idx] != b[idx] {
+      dist++
+    }
+  }
+
+  return
 }
