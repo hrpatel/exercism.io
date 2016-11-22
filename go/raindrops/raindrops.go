02d114811314ee4e1c9bb3b8b50a2c419a8090fe
hrpatel <pate3900+github@gmail.com>
2016-11-21 22:21:49 -0500

    implement raindrops solution

--- go/raindrops/raindrops.go
+++ go/raindrops/raindrops.go
@@ -1,7 +1,31 @@
 package raindrops
 
+import (
+  "fmt"
+)
+
 const testVersion = 2
 
-func Convert(int) string
+func Convert(input int) (sound string) {
 
-// The test program has a benchmark too.  How fast does your Convert convert?
+  // define the sound mapping
+  var sounds = map[int]string {
+    3: "Pling",
+    5: "Plang",
+    7: "Plong",
+  }
+
+  // check for factor and add to the sound
+  for _, x := range []int{3,5,7} {
+    if input % x == 0 {
+      sound = sound + sounds[x]
+    }
+  }
+
+  // no sound was made so lets pass the input through
+  if sound == "" {
+    sound = fmt.Sprintf("%d", input)
+  }
+
+  return
+}
