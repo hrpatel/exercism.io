30b91b22b9762efee993b9999a29a0e2aa447064
hrpatel <pate3900+github@gmail.com>
2016-11-30 19:24:14 -0500

    taking another approach

--- go/acronym/acronym.go
+++ go/acronym/acronym.go
@@ -4,24 +4,32 @@ package acronym
 import (
   "fmt"
   "strings"
+  "unicode"
 )
 
 const testVersion = 1
 
 func abbreviate (input string) (abbr string) {
 
-  // Return true if ' ', ',', ':', '-'
-  f := func(c rune) bool {
-    return c == ',' || c == ':' || c == ' ' || c == '-'
+  runes := []rune(input)
+
+  // loop through the string one rune at a time
+  for i, r := range runes {
+
+    // first letter gets abbreviated
+    if i == 0 {
+      abbr = abbr + string(r)
+      continue
+    }
+
+    pr := runes[i-1]
+    if ( unicode.IsLower(pr) && unicode.IsUpper(r) ) {
+      abbr = abbr + string(r)
+    }
+
   }
 
-  parts := strings.FieldsFunc(input, f)
-  fmt.Println(parts)
-
-  for _, part := range parts {
-    fmt.Println(string(part[0]))
-    abbr = abbr + strings.ToUpper(string(part[0]))
-  }
-
+  abbr = strings.ToUpper(abbr)
+  fmt.Println(abbr)
   return
 }
